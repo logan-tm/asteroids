d038b524e55008a3aee4f07500f43bebe6d47166
logan-tm <logan@logantm.com>
2024-10-24 13:04:24 -0600

    D: add rotation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         return
     screen.fill("black")
     player.draw(screen)
-
+    player.update(dt)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000 # save delta time as seconds from ms
