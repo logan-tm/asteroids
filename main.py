5672d604d6a73b446e11e487f4ae37cf97b57338
logan-tm <logan@logantm.com>
2024-10-24 13:14:05 -0600

    F: add groups

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ def main():
   # print(f"Screen width: {SCREEN_WIDTH}")
   # print(f"Screen height: {SCREEN_HEIGHT}")
   pygame.init()
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = updatable, drawable
   clock = pygame.time.Clock()
   dt = 0 # seconds
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  # updatable.add(player)
+  # drawable.add(player)
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill("black")
-    player.draw(screen)
-    player.update(dt)
+    for u in updatable:
+      u.update(dt)
+    for d in drawable:
+      d.draw(screen)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000 # save delta time as seconds from ms
